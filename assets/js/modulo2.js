6f33c257d9659e1f07061e5eedf02bc71f43c217
Gustavo Felipe Morais <gutibrk74@gmail.com>
2024-11-20 16:09:22 -0300

    Update modulo2.js

--- assets/js/modulo2.js
+++ assets/js/modulo2.js
@@ -1,8 +1,26 @@
-const accordions = document.querySelectorAll('.accordion-modulo2');
+var acc = document.getElementsByClassName("accordion-2");
+var i;
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', () => {
-        const body = accordion.querySelector('.accordion-body-modulo2');
-        body.classList.toggle('active');
-    })
-})
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    // Adicionar a classe de transição
+    this.classList.add("expanding");
+
+    // Remover a classe de transição após 300ms (mesmo tempo da transição CSS)
+    setTimeout(() => {
+      this.classList.remove("expanding");
+    }, 300);
+
+    // Alternar a exibição do painel
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}

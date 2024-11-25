1339b70c3c412b1a49c4729c08cd067e144bf537
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2024-11-24 22:34:11 -0300

    Delete API-OsPyOn directory

--- API-OsPyOn/static/js/modulo2.js
+++
@@ -1,8 +0,0 @@
-const accordions = document.querySelectorAll('.accordion2');
-
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', () => {
-        const body = accordion.querySelector('.accordion-body2');
-        body.classList.toggle('active');
-    })
-})
